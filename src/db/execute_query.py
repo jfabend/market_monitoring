243455ab0a1655bdc460845287a5ef7d8a1e3dc5
Jonathan Abend <jonathan.abend@gmail.com>
2020-04-13 16:07:19 +0200

    execution of queries works now

---
+++ src/db/execute_query.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+class QueryExecution():
+
+    def __init__(self, conn, cur):
+        self.conn = conn
+        self.cur = cur
+
+    def execute_query(self, query_string):
+        #try:
+        #    conn = psycopg2.connect(database = "projetofinal", user = "postgres", password = "admin", host = "localhost", port = "5432")
+        #except:
+        #    print("I am unable to connect to the database") 
+
+        #cur = conn.cursor()
+        try:
+            self.cur.execute(query_string)
+        except:
+            print("I can't drop our test database!")
+
+        self.conn.commit() # <--- makes sure the change is shown in the database
+        self.conn.close()
+        self.cur.close()
--- src/run_db_import.py
+++ src/run_db_import.py
@@ -1,9 +1,17 @@
 from dotenv import load_dotenv
 load_dotenv(verbose=False)
 from db.connect_db import DbConnection
+from db.execute_query import QueryExecution
 import os
 
 dbini_path = os.getenv("ROOT_DIR")
 
 _DbConnection = DbConnection(dbini_path)
-print(str(_DbConnection.setup_connection()))
+connection_objects = _DbConnection.setup_connection()
+conn = connection_objects[0]
+cur = connection_objects[1]
+
+query_string = "CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);"
+
+_QueryExecution = QueryExecution(conn, cur)
+_QueryExecution.execute_query(query_string)
