dac7ae33526f16a05c3a4c115bece5c761611119
Jonathan Abend <jonathan.abend@gmail.com>
2020-04-13 11:10:36 +0200

    added class for database connection and dotenv

--- src/db/connect_db.py
+++ src/db/connect_db.py
@@ -4,25 +4,36 @@
 # parameters as a dictionary. This function will be imported in to the main python script:
 
 #!/usr/bin/python
+
 from configparser import ConfigParser
- 
-def config(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
- 
-    # get section, default to postgresql
-    db = {}
+
+class DbConnection():
+
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.dbini_path = root_dir + '\db\database.ini'
+
+    def config(self):
+        # create a parser
+        parser = ConfigParser()
+        filename = self.dbini_path
+        section='postgresql'
+        # read config file
+        parser.read(filename)
     
-    # Checks to see if section (postgresql) parser exists
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-         
-    # Returns an error if a parameter is called that is not listed in the initialization file
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
- 
-    return db
+        # get section, default to postgresql
+        db = {}
+        
+        # Checks to see if section (postgresql) parser exists
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+            
+        # Returns an error if a parameter is called that is not listed in the initialization file
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    
+        return db
+
+#print(str(self.config(filename=self.dbini_path , section='postgresql')))
