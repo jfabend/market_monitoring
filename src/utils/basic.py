4b27a192d1e6990267b985c7d74455d131dbffc4
Jonathan Abend <jonathan.abend@gmail.com>
2020-04-18 19:31:47 +0200

    added get_file_headers to utils.basic

--- src/utils/basic.py
+++ src/utils/basic.py
@@ -1,4 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import glob
+import csv
+import string
+import re
 
 def get_folder_files(foldername, pattern = '*'):
     return glob.glob(foldername + pattern)
+
+def get_parent_folder(path):
+    if "\\" in path:
+        path_components = path.split("\\")
+    if "/" in path:
+        path_components = path.split("/")
+    return path_components[-2]
+
+def get_file_headers(path):
+    #printable = set(string.printable)
+    with open(path, 'r') as f:
+        d_reader = csv.DictReader(f)
+        header = d_reader.fieldnames
+        header_decoded = []
+        for col in header:
+            col_only_letters = re.sub(r'[^a-zA-Z]',r'', col.replace("\W", ""))
+            if not col_only_letters:
+                col_only_letters = 'leer'
+            header_decoded.append(col_only_letters)
+    return header_decoded
