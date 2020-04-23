e300c0fa7913e8c2bda3af88544ba46e23cb15eb
Jonathan Abend <jonathan.abend@gmail.com>
2020-04-23 11:12:07 +0200

    cmd r yahoo api download works now

--- src/retrieve_data.R
+++ src/retrieve_data.R
@@ -1,15 +1,20 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+cat(paste0(c("Argument 1: ", args[1]), collapse = "") , sep = "\n")
+cat(paste0(c("Argument 2: ", args[2]), collapse = "") , sep = "\n")
+
+#args = "dax"
+#setwd(".")
+
 
 library(quantmod)
 library(data.table)
-library(readxl)
-library(tidyverse)
 
-symbol_temp <- "^SP500FTR"
-getSymbols(symbol_temp, src="yahoo")
-kurs_sp_f <- data.table(index(get("SP500FTR")), get("SP500FTR")) 
+symbol_temp <- toupper(args[1])
+target_path <- paste0(c(args[2], "/", symbol_temp), collapse = "")
 
-
-
-symbol_temp <- "NFLX"
 getSymbols(symbol_temp, src="yahoo")
 kurs <- data.table(index(get(symbol_temp)), get(symbol_temp)) 
+filename <- paste0(c(target_path,"/", symbol_temp, ".csv"), collapse = "")
+dir.create(target_path, showWarnings = FALSE)
+write.csv(kurs, filename, row.names = F)
