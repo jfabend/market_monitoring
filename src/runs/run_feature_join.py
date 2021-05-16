85de9f5ded47b40442414c566178e12552231da4
Jonathan Abend <jonathan.abend@gmail.com>
2021-05-16 16:33:02 +0200

    added us unemployment rate data

--- src/runs/run_feature_join.py
+++ src/runs/run_feature_join.py
@@ -6,13 +6,11 @@ sys.path.append(os.getenv("ROOT_DIR"))
 from utils import basic
 from db.join_features import FeatureJoiner
 
-feature_cols = ["c_usa_bond_10year.open",
-                "c_usa_bond_2year.open",
-                "c_vix.high",
-                #"c_ted_spread_credit_risk.value",
-                "c_leitzins_usa.value",
-                "c_sp500.volume",
+feature_cols = ["c_capacity_utilization.value",
+                "c_industrial_production.value",
+                "c_fed_funds_rate.value",
+                "c_us_national_unemployment_rate.value",
                 "c_sp500.high"]
 
 _FeatureJoiner = FeatureJoiner()
-_FeatureJoiner.feature_join(feature_cols, "basis_use_case_zwei")
+_FeatureJoiner.feature_join(feature_cols, "pre_features_20210516")
